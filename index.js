3d7f0748f04f0381b0ac563c3694a1a5e11f095d
Nikachu <mindgamer2012@gmail.com>
2023-03-01 21:20:43 +0000

    [add] 完成

--- index.js
+++ index.js
@@ -1,20 +1,310 @@
-import {JSDOM} from 'jsdom'
+import { JSDOM } from 'jsdom'
 import fetch from 'node-fetch'
+import fs from 'fs'
 
-const pageURL = 'https://syllabus.kosen-k.go.jp/Pages/PublicSubjects?school_id=22&department_id=15&year=2022&lang=ja'
+const pageURL = 'http://localhost:3000/origin.html'
 
 console.log(`kosen-syllabus-parser v1.0.0`)
 console.log(`(C) ${new Date().getFullYear()} nikachu2012`)
 const parse = async () => {
+    const start = performance.now();
+
+    // ファイルDL
+    console.log(`${pageURL} download ${performance.now() - start}ms`)
     const res = await fetch(pageURL);
     const body = await res.text();
+    console.log(`success. ${performance.now() - start}ms`)
 
+    // DOM生成
+    console.log(`DOM create`)
     const dom = new JSDOM(body);
-    console.log(body)
-
     const document = dom.window.document
+    console.log(`success ${performance.now() - start}ms`)
 
-    console.log(document.querySelector('#sytablenc').outerHTML)
+
+
+    // パースJSON生成
+    console.log(`parseData create`)
+
+    let parse = {
+        school: document.querySelector(`.breadcrumb`).childNodes[3].textContent.trim(),
+        department: document.querySelector(`.col-xs-4`).textContent.trim(),
+        date: new Date().getTime(),
+        url: pageURL,
+
+        subject: [],
+
+        teacher: {},
+    };
+
+    const yearJPN = document.querySelectorAll(`.btn-group`)[1].childNodes[1].textContent.trim()
+    if (yearJPN.match(/^..(?=[0-9][0-9]年度)/)[0] == '令和') {
+        parse.year = 2018 + parseFloat(yearJPN.match(/(?<=^..)[0-9][0-9](?=年度)/)[0]);
+    }
+    else if (yearJPN.match(/^..(?=[0-9][0-9]年度)/)[0] == '平成') {
+        parse.year = 2019 + (parseFloat(yearJPN.match(/(?<=^..)[0-9][0-9](?=年度)/)[0]) - 31);
+    }
+    else {
+        console.error(`can't parse year`)
+    }
+
+    const subjectList = document.querySelectorAll(`#sytablenc tr[data-course-value]`);
+
+
+    //TODO: parse.subjectList = subjectList;
+
+    subjectList.forEach((e, i) => {
+        //e=科目ごとのDOM
+        //i=index
+        const subjectNameList = document.querySelectorAll('.mcc-show')[i]
+
+        let subjectData = {
+            category: {},
+            credit: {},
+            class: {
+                semester: {},
+                quarter: {},
+            },
+            teacher: {},
+        }
+
+        // コースの区分
+        const typeText = e.children[0].textContent.trim()
+        const takeText = e.children[1].textContent.trim()
+        subjectData.category.typeText = typeText;
+        subjectData.category.takeText = takeText;
+
+        if (takeText == '必修' || takeText == '集中') {
+            subjectData.category.take = true;
+        }
+        else if (takeText == '選択') {
+            subjectData.category.take = false;
+        }
+        else {
+            subjectData.category.take = false;
+        }
+
+        // 科目名
+        subjectData.title = subjectNameList.textContent.trim();
+
+        var location = new URL(pageURL);
+
+        // 詳細ページのURL取得
+        if (subjectNameList.tagName == 'A') {
+            subjectData.description = location.protocol + '//' + location.hostname + subjectNameList.getAttribute('href');
+            subjectData.descVisibility = true;
+        }
+        else if (subjectNameList.tagName == "DIV") {
+            subjectData.description = location.protocol + '//' + location.hostname + document.querySelectorAll('.mcc-hide')[i].getAttribute('href');
+            subjectData.descVisibility = false;
+        }
+        else {
+            subjectData.description = null;
+            subjectData.descVisibility = false;
+        }
+
+        // 科目番号
+        subjectData.code = e.children[3].textContent.trim();
+
+        // 単位
+        const creditType = e.children[4].textContent.trim();
+        if (creditType == "履修単位") {
+            subjectData.credit.kosen = true;
+        }
+        else if (creditType == "学修単位") {
+            subjectData.credit.kosen = false;
+        }
+        else {
+            subjectData.credit.kosen = false;
+        }
+        subjectData.credit.text = creditType;
+        subjectData.credit.count = parseFloat(e.children[5].textContent.trim());
+
+        // 授業時数
+        if (e.children[6].textContent.trim() !== '' || e.children[7].textContent.trim() !== '' || e.children[8].textContent.trim() !== '' || e.children[9].textContent.trim() !== '') {
+            subjectData.class.grade = 1;
+
+            const firstQuarter = Number(e.children[6].textContent.trim(), 10);
+            const secondQuarter = Number(e.children[7].textContent.trim(), 10);
+            const thirdQuarter = Number(e.children[8].textContent.trim(), 10);
+            const fourthQuarter = Number(e.children[9].textContent.trim(), 10)
+
+            subjectData.class.hour = firstQuarter + secondQuarter + thirdQuarter + fourthQuarter
+
+            subjectData.class.semester.first = firstQuarter + secondQuarter;
+            subjectData.class.semester.second = thirdQuarter + fourthQuarter;
+
+            if (e.children[6].colSpan == 2) {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = null;
+            }
+            else {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = secondQuarter;
+            }
+
+            if (e.children[8].colSpan == 2) {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = null;
+            }
+            else {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = fourthQuarter;
+            }
+        }
+        else if (e.children[10].textContent.trim() !== '' || e.children[11].textContent.trim() !== '' || e.children[12].textContent.trim() !== '' || e.children[13].textContent.trim() !== '') {
+            subjectData.class.grade = 2;
+
+            const firstQuarter = Number(e.children[10].textContent.trim(), 10);
+            const secondQuarter = Number(e.children[11].textContent.trim(), 10);
+            const thirdQuarter = Number(e.children[12].textContent.trim(), 10);
+            const fourthQuarter = Number(e.children[13].textContent.trim(), 10)
+
+            subjectData.class.hour = firstQuarter + secondQuarter + thirdQuarter + fourthQuarter
+
+            subjectData.class.semester.first = firstQuarter + secondQuarter;
+            subjectData.class.semester.second = thirdQuarter + fourthQuarter;
+
+            if (e.children[10].colSpan == 2) {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = null;
+            }
+            else {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = secondQuarter;
+            }
+
+            if (e.children[12].colSpan == 2) {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = null;
+            }
+            else {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = fourthQuarter;
+            }
+        }
+        else if (e.children[14].textContent.trim() !== '' || e.children[15].textContent.trim() !== '' || e.children[16].textContent.trim() !== '' || e.children[17].textContent.trim() !== '') {
+            subjectData.class.grade = 3;
+
+            const firstQuarter = Number(e.children[14].textContent.trim(), 10);
+            const secondQuarter = Number(e.children[15].textContent.trim(), 10);
+            const thirdQuarter = Number(e.children[16].textContent.trim(), 10);
+            const fourthQuarter = Number(e.children[17].textContent.trim(), 10)
+
+            subjectData.class.hour = firstQuarter + secondQuarter + thirdQuarter + fourthQuarter
+
+            subjectData.class.semester.first = firstQuarter + secondQuarter;
+            subjectData.class.semester.second = thirdQuarter + fourthQuarter;
+
+            if (e.children[14].colSpan == 2) {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = null;
+            }
+            else {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = secondQuarter;
+            }
+
+            if (e.children[16].colSpan == 2) {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = null;
+            }
+            else {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = fourthQuarter;
+            }
+        }
+        else if (e.children[18].textContent.trim() !== '' || e.children[19].textContent.trim() !== '' || e.children[20].textContent.trim() !== '' || e.children[21].textContent.trim() !== '') {
+            subjectData.class.grade = 4;
+
+            const firstQuarter = Number(e.children[18].textContent.trim(), 10);
+            const secondQuarter = Number(e.children[19].textContent.trim(), 10);
+            const thirdQuarter = Number(e.children[20].textContent.trim(), 10);
+            const fourthQuarter = Number(e.children[21].textContent.trim(), 10)
+
+            subjectData.class.hour = firstQuarter + secondQuarter + thirdQuarter + fourthQuarter
+
+            subjectData.class.semester.first = firstQuarter + secondQuarter;
+            subjectData.class.semester.second = thirdQuarter + fourthQuarter;
+
+            if (e.children[18].colSpan == 2) {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = null;
+            }
+            else {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = secondQuarter;
+            }
+
+            if (e.children[20].colSpan == 2) {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = null;
+            }
+            else {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = fourthQuarter;
+            }
+        }
+        else if (e.children[22].textContent.trim() !== '' || e.children[23].textContent.trim() !== '' || e.children[24].textContent.trim() !== '' || e.children[25].textContent.trim() !== '') {
+            subjectData.class.grade = 5;
+
+            const firstQuarter = Number(e.children[22].textContent.trim(), 10);
+            const secondQuarter = Number(e.children[23].textContent.trim(), 10);
+            const thirdQuarter = Number(e.children[24].textContent.trim(), 10);
+            const fourthQuarter = Number(e.children[25].textContent.trim(), 10)
+
+            subjectData.class.hour = firstQuarter + secondQuarter + thirdQuarter + fourthQuarter
+
+            subjectData.class.semester.first = firstQuarter + secondQuarter;
+            subjectData.class.semester.second = thirdQuarter + fourthQuarter;
+
+            if (e.children[22].colSpan == 2) {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = null;
+            }
+            else {
+                subjectData.class.quarter.first = firstQuarter;
+                subjectData.class.quarter.second = secondQuarter;
+            }
+
+            if (e.children[24].colSpan == 2) {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = null;
+            }
+            else {
+                subjectData.class.quarter.third = thirdQuarter;
+                subjectData.class.quarter.fourth = fourthQuarter;
+            }
+        }
+
+        // 担当教員
+        const teacherText = e.children[26].textContent.trim();
+        const teacherList = teacherText.split(',');
+        subjectData.teacher.text = teacherText;
+        subjectData.teacher.list = teacherList
+
+        teacherList.forEach((e, i) => {
+            if(parse.teacher[e] == undefined){
+                parse.teacher[e] = {};
+                parse.teacher[e].subject = {};
+                parse.teacher[e].subject.all = [];
+                parse.teacher[e].subject.grade = [[],[],[],[],[]];
+            }
+            parse.teacher[e].url = `https://research.kosen-k.go.jp/plugin/rmaps/index/11/122?name=${encodeURIComponent(e).replace('%20', '+')}&area=A04&affiliation=6600`
+            parse.teacher[e].subject.all.push(subjectNameList.textContent.trim())
+            parse.teacher[e].subject.grade[subjectData.class.grade - 1].push(subjectNameList.textContent.trim())
+        })
+
+        // 履修上の区分
+        subjectData.division = e.children[27].textContent.trim();
+        
+        parse.subject.push(subjectData)
+    })
+
+    console.log(`success ${performance.now() - start}ms`)
+
+    fs.writeFile('output.json', JSON.stringify(parse, null, '    '), (err) => { console.error(err) });
+    console.log('data saved.')
 }
 
 parse();
